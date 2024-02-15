c5b66f588d5158df30b825f355bf35bb732a8a31
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-02-15 18:27:33 +0100

    last commit

---
+++ src/main/java/es/uniovi/avib/morphing/projections/backend/controller/OrganizationController.java
@@ -0,0 +1,104 @@
+package es.uniovi.avib.morphing.projections.backend.controller;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+
+import es.uniovi.avib.morphing.projections.backend.service.OrganizationService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping(value="/organizations", produces = MediaType.APPLICATION_JSON_VALUE)
+public class OrganizationController {
+	private OrganizationService organizationService;
+	
+	@RequestMapping(method = { RequestMethod.GET }, produces = "application/json")
+	public String findAll() {
+		log.info("find all annotations from controller");
+        
+        Object result = organizationService.findAll();
+        
+        Gson gson = new Gson();
+        String response = gson.toJson(result, List.class);
+                
+        return response;			
+	}
+
+	@RequestMapping(method = { RequestMethod.GET }, produces = "application/json", value = "/{organizationId}")	
+	public String findById(@PathVariable String organizationId) {
+		log.info("find by id {} organizations from controller", organizationId);
+		
+		Object result = organizationService.findById(organizationId);
+													
+        Gson gson = new Gson();
+        String response = gson.toJson(result, Object.class);
+                
+        return response;	
+	}
+	
+	@RequestMapping(method = { RequestMethod.POST }, produces = "application/json")	
+	public String save(@RequestBody Object organization) {		
+		Object result = organizationService.save(organization);
+
+		//log.debug("save: create/update annotation with annotationId: {} from Manager Service", organization.getOrganizationId());
+			
+        Gson gson = new Gson();
+        String response = gson.toJson(result, Object.class);
+        
+        return response;			
+	}
+
+	@RequestMapping(method = { RequestMethod.DELETE },value = "/{organizationId}")	
+	public void deleteById(@PathVariable String organizationId) {		
+		log.debug("deleteById: remove annotation with annotationId: {}", organizationId);
+			
+		organizationService.deleteById(organizationId);					
+	}	
+	
+	@RequestMapping(method = { RequestMethod.GET }, produces = "application/json", value = "/aggregate")
+	public String findAllAggregate() {
+		log.info("find all annotations from controller");
+        
+        Object result = organizationService.findAllAggregate();
+        
+        Gson gson = new Gson();
+        String response = gson.toJson(result, List.class);
+                
+        return response;			
+	}
+	
+	@RequestMapping(method = { RequestMethod.GET }, produces = "application/json", value = "/{organizationId}/aggregate")	
+	public String findByIdAggregate(@PathVariable String organizationId) {
+		log.info("find by id {} organizations from controller", organizationId);
+		
+		Object result = organizationService.findByIdAggregate(organizationId);
+													
+        Gson gson = new Gson();
+        String response = gson.toJson(result, Object.class);
+                
+        return response;	
+	}
+	
+	@RequestMapping(method = { RequestMethod.GET }, produces = "application/json", value = "/users/{userId}/aggregate")	
+	public String findByUserAggregate(@PathVariable String userId) {
+		log.info("find by id {} organizations from controller", userId);
+		
+		Object result = organizationService.findByUserAggregate(userId);
+													
+        Gson gson = new Gson();
+        String response = gson.toJson(result, Object.class);
+                
+        return response;	
+	}	
+}
---
+++ src/main/java/es/uniovi/avib/morphing/projections/backend/service/OrganizationService.java
@@ -0,0 +1,103 @@
+package es.uniovi.avib.morphing.projections.backend.service;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import es.uniovi.avib.morphing.projections.backend.configuration.OrganizationConfig;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class OrganizationService {	
+	private RestTemplate restTemplate;
+	
+	private OrganizationConfig organizationConfig;
+	
+	public Object findAll() {
+		log.info("find all organizations from service");
+				
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/organizations";
+		
+		ResponseEntity<Object> responseEntityStr = restTemplate.getForEntity(url, Object.class);
+		
+		return responseEntityStr.getBody();		
+	}
+	
+	public Object findById(String organizationId) {
+		log.debug("findById: found organization with id {} from service", organizationId);
+		
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/organizations/" + organizationId;
+		
+		ResponseEntity<Object> responseEntityStr = restTemplate.getForEntity(url, Object.class);
+		
+		return responseEntityStr.getBody();
+	}
+	
+	public Object save(Object organization) {
+		log.debug("save annotation from service");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/organizations";
+			
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		HttpEntity entity = new HttpEntity(organization ,headers);
+		  
+		ResponseEntity<Object> responseEntityStr = restTemplate.exchange(url, HttpMethod.POST, entity, Object.class);
+		
+		return responseEntityStr.getBody();
+	}
+	
+	public void deleteById(String organizationId) {
+		log.debug("delete organization from service");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/organizations";
+			
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		HttpEntity entity = new HttpEntity(organizationId ,headers);
+		  
+		restTemplate.exchange(url, HttpMethod.DELETE, entity, Object.class);
+	}
+	
+	public Object findAllAggregate() {
+		log.info("find all organizations from aggregate service");
+				
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/organizations/aggregate";
+		
+		ResponseEntity<Object> responseEntityStr = restTemplate.getForEntity(url, Object.class);
+		
+		return responseEntityStr.getBody();		
+	}
+	
+	public Object findByIdAggregate(String organizationId) {
+		log.debug("findById: found organization with id {} from aggregate service", organizationId);
+		
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/organizations/" + organizationId + "/aggregate";
+		
+		ResponseEntity<Object> responseEntityStr = restTemplate.getForEntity(url, Object.class);
+		
+		return responseEntityStr.getBody();
+	}	
+	
+	public Object findByUserAggregate(String userId) {
+		log.debug("findById: found user with id {} from aggregate service", userId);
+		
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/organizations/users/" + userId + "/aggregate";
+		
+		ResponseEntity<Object> responseEntityStr = restTemplate.getForEntity(url, Object.class);
+		
+		return responseEntityStr.getBody();
+	}	
+}
