4114260eb621cbc165c45f333f21640b6f23184d
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-08-14 19:34:57 +0200

    last commit

---
+++ src/main/java/es/uniovi/avib/morphing/projections/backend/controller/CaseController.java
@@ -0,0 +1,66 @@
+package es.uniovi.avib.morphing.projections.backend.controller;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import es.uniovi.avib.morphing.projections.backend.service.CaseService;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping(value="/cases", produces = MediaType.APPLICATION_JSON_VALUE)
+public class CaseController {
+	private CaseService caseService;
+	
+	@RequestMapping(method = { RequestMethod.GET }, produces = "application/json")
+	public String findAll() {
+		log.info("find all cases from controller");
+        
+        Object result = caseService.findAll();
+        
+        Gson gson = new Gson();
+        String response = gson.toJson(result, List.class);
+                
+        return response;			
+	}
+	
+	@RequestMapping(method = { RequestMethod.GET }, produces = "application/json", value = "/{caseId}")	
+	public String findById(@PathVariable String caseId) {
+		log.info("find by id {} case from controller", caseId);
+		
+		Object result = caseService.findById(caseId);
+													
+        Gson gson = new Gson();
+        String response = gson.toJson(result, Object.class);
+                
+        return response;	
+	}
+	
+	@RequestMapping(method = { RequestMethod.POST }, produces = "application/json")	
+	public String save(@RequestBody Object _case) {		
+		Object organizationSaved = caseService.save(_case);
+			
+        Gson gson = new Gson();
+        String response = gson.toJson(organizationSaved, Object.class);
+        
+        return response;			
+	}
+
+	@RequestMapping(method = { RequestMethod.DELETE },value = "/{organizationId}")	
+	public void deleteById(@PathVariable String caseId) {		
+		log.debug("deleteById: remove case with caseId: {}", caseId);
+			
+		caseService.deleteById(caseId);					
+	}
+}
---
+++ src/main/java/es/uniovi/avib/morphing/projections/backend/service/CaseService.java
@@ -0,0 +1,73 @@
+package es.uniovi.avib.morphing.projections.backend.service;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import es.uniovi.avib.morphing.projections.backend.configuration.OrganizationConfig;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class CaseService {	
+	private RestTemplate restTemplate;
+	
+	private OrganizationConfig organizationConfig;
+	
+	public Object findAll() {
+		log.info("find all cases from service");
+				
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/cases";
+		
+		ResponseEntity<Object> responseEntityStr = restTemplate.getForEntity(url, Object.class);
+		
+		return responseEntityStr.getBody();		
+	}
+
+	public Object findById(String caseId) {
+		log.debug("findById: found case with id {} from service", caseId);
+		
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/cases/" + caseId;
+		
+		ResponseEntity<Object> responseEntityStr = restTemplate.getForEntity(url, Object.class);
+		
+		return responseEntityStr.getBody();
+	}
+	
+	public Object save(Object _case) {
+		log.debug("save case from service");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/cases";
+			
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		HttpEntity entity = new HttpEntity(_case ,headers);
+		  
+		ResponseEntity<Object> responseEntityStr = restTemplate.exchange(url, HttpMethod.POST, entity, Object.class);
+		
+		return responseEntityStr.getBody();
+	}
+	
+	public void deleteById(String caseId) {
+		log.debug("delete case from service");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		String url = "http://" + organizationConfig.getHost() + ":" + organizationConfig.getPort() + "/cases";
+			
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		HttpEntity entity = new HttpEntity(caseId ,headers);
+		  
+		restTemplate.exchange(url, HttpMethod.DELETE, entity, Object.class);
+	}
+}
